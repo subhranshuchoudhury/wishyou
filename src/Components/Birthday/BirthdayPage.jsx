87ae3866779bbe02d23fc91dd2f3e9ab43f1b974
subhranshuchoudhury <subhransuchoudhury00@gmail.com>
2022-07-09 22:02:22 +0530

    fix

--- src/Components/Birthday/BirthdayPage.jsx
+++ src/Components/Birthday/BirthdayPage.jsx
@@ -4,14 +4,20 @@ import './birthday.css';
 
 function BirthdayPage(props) {
 
-  const [names, setnames] = useState({field1:"",field2:""});
+  const [names, setnames] = useState({ field1: "", field2: "" });
   const [link, setlink] = useState("");
-  const handleParameters = (event)=> {
-    const {name, value} = event.target;
-        setnames((prev) =>{
-            return {...prev,[name]: value};
-        });
-        setlink(`?name=${names.field1}&&from=${names.field2}`)
+  const [visible,setVisible] = useState(false);
+  const handleParameters = (event) => {
+    const { name, value } = event.target;
+    setnames((prev) => {
+      return { ...prev, [name]: value };
+    });
+  }
+
+  const createLink = () => {
+    setlink(`?name=${names.field1}&&from=${names.field2}`)
+    setVisible(true);
+
   }
 
   return (
@@ -44,19 +50,21 @@ function BirthdayPage(props) {
         </div>
       </div>
       <div className="text">Touch The Cake <br></br>May all your dreams come true and May God crown you with all the success in life. Wishing you many returns of this day. Happy birthday {props.name}! From, {props.from}.
-      <br></br>
-      <input type="text"  name='field1' value={names.field1} onChange={handleParameters} placeholder='To..'></input>
-      <input type="text"  name='field2' value={names.field2} onChange={handleParameters} placeholder='From..'></input>
-      {
-        names.field1 && names.field2 ? <a href={"whatsapp://send?text=https://subhranshuchoudhury.github.io/wishyou"+link} data-action="share/whatsapp/share">Share via Whatsapp</a> : null
-      }
-      
+        <br></br>
+        <input type="text" name='field1' value={names.field1} onChange={handleParameters} placeholder='To..'></input>
+        <input type="text" name='field2' value={names.field2} onChange={handleParameters} placeholder='From..'></input>
+        {
+          visible ? null : <button onClick={createLink}>Create Link</button>
+        }
+        {
+          visible ? <a href={"whatsapp://send?text=https://subhranshuchoudhury.github.io/wishyou" + link} data-action="share/whatsapp/share">Share via Whatsapp</a> : null
+        }
       </div>
 
       <audio id="HBD" loop>
         <source src="https://wishesplus.com/wp-content/uploads/2021/02/Happy-Birthday-To-You-Happy-Birthday-Songs-2021-WishesPlus.com_.mp3"></source>
       </audio>
-    
+
 
     </div>
   );
