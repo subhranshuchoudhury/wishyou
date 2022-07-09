d7b6e7faaf54cbb80bdd8043b58c80decabc6b0b
subhranshuchoudhury <subhransuchoudhury00@gmail.com>
2022-07-09 12:00:45 +0530

    probably commit

--- src/Components/Birthday/BirthdayPage.jsx
+++ src/Components/Birthday/BirthdayPage.jsx
@@ -11,7 +11,7 @@ function BirthdayPage(props) {
         setnames((prev) =>{
             return {...prev,[name]: value};
         });
-        setlink(`?names=${names.field1}&&from=${names.field2}`)
+        setlink(`?from=${names.field2}&&name=${names.field1}`)
   }
 
   return (
